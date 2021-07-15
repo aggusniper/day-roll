35306ec4897066f332f8fcb5f87acb3c95a3b036
aggusniper <aggusniper@gmail.com>
2021-07-15 10:33:31 +0300

    create README file

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -36,11 +36,21 @@ class Form extends Component {
 
         // 1: to check the input field
 
+        // also we want the enter key to work with the add button
+
+        // document.addEventListener('keypress', (e) => {
+        //     if(e.keyCode === 13 || e.which === 13) {
+        //         console.log('Enter key was pressed!');
+        //     }
+        // })
+
         if (this.state.roll !== '' && this.state.time !== '') {
+            console.log(this.state.time);
             this.setState({
                 roll: '',
                 time: ''
             });
+
             return this.props.handleChange(this.state);
         }
     } 
@@ -70,14 +80,15 @@ class Form extends Component {
                     type="time" 
                     name="roll"
                     value={time}
-                    placeholder="e.g Time"
+                    placeholder='hh:mm' 
                     className="input-time"
                     onChange={this.handleTime}
                     required
+                    step="2"
                 />
                 <button type="submit" className="add-btn">
                     <article>
-                        <img src={addBtn} alt="add button" onClick={this.submitForm} />
+                        <img src={addBtn} alt="add button" onKeyPress={this.add} onClick={this.submitForm} />
                         <p>Add</p>
                     </article>
                 </button>
